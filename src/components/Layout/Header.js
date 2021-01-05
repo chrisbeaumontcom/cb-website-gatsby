e9f5f555b2c0b8fcff404bfa6aa30b5cb8c953d4
Chris Beaumont <chris@chrisbeaumont.com>
2021-01-05 17:20:47 +1100

    Fix css layout flashing remove styled components

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,53 +5,9 @@ import banner01 from '../../assets/img/layout/banner01zucchinis.jpg';
 import banner02 from '../../assets/img/layout/banner02lemons.jpg';
 import banner03 from '../../assets/img/layout/banner03artichokes.jpg';
 import banner04 from '../../assets/img/layout/banner04cherries.jpg';
-import styled from 'styled-components';
-
-const HeaderStyles = styled.div`
-  div.container-fluid {
-    background-color: #000000;
-    padding: 0;
-  }
-  div.headbanner {
-    background-color: #000000;
-    padding: 0;
-  }
-  div.carousel-item img {
-    width: 100%;
-    height: auto;
-  }
-  img.imgheader {
-    width: 100%;
-    height: auto;
-  }
-  .banner a:hover {
-    text-decoration: none;
-  }
-  .banner h1 {
-    color: #ffffff;
-    margin: 50px;
-    margin-bottom: 2px;
-    font-size: 1.6em;
-    font-family: Quicksand, Arial, Helvetica, sans-serif;
-    font-weight: 300;
-  }
-  .banner h2 {
-    color: #ffffff;
-    margin: 50px;
-    margin-top: 2px;
-    font-size: 1.1em;
-    font-family: Quicksand, Arial, Helvetica, sans-serif;
-    font-weight: 300;
-  }
-  @media only screen and (max-width: 650px) {
-    .banner h1 {
-      margin: 15px 20px 4px 20px;
-    }
-    .banner h2 {
-      margin: 2px 20px 10px 20px;
-    }
-  }
-`;
+//import styled from 'styled-components';
+// const HeaderStyles = styled.div`
+// `;
 
 export default function Header(props) {
   const isHome = props.title === 'Home';
@@ -62,52 +18,94 @@ export default function Header(props) {
   }
 
   return (
-    <HeaderStyles>
-      <div className="container-fluid">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-md-8 col-sm-6 col-xs-12 banner">
-              <Link to="/">
-                <h1>{props.title}</h1>
-                <h2>{props.subtitle}</h2>
-              </Link>
-            </div>
+    <div
+      className="container-fluid"
+      style={{ backgroundColor: `#000000`, padding: `0` }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 col-md-8 col-sm-6 col-xs-12 banner">
+            <Link to="/">
+              <h1>{props.title}</h1>
+              <h2>{props.subtitle}</h2>
+            </Link>
+          </div>
 
-            <div className={classList}>
-              <Carousel controls={false} fade={true} indicators={false}>
-                <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src={banner01}
-                    alt="First slide: Zucchinis"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src={banner02}
-                    alt="Second slide: Lemons"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src={banner03}
-                    alt="Third slide:Artichokes"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src={banner04}
-                    alt="Fourth slide: Cherries"
-                  />
-                </Carousel.Item>
-              </Carousel>
-            </div>
+          <div className={classList}>
+            <Carousel controls={false} fade={true} indicators={false}>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={banner01}
+                  alt="First slide: Zucchinis"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={banner02}
+                  alt="Second slide: Lemons"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={banner03}
+                  alt="Third slide:Artichokes"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={banner04}
+                  alt="Fourth slide: Cherries"
+                />
+              </Carousel.Item>
+            </Carousel>
           </div>
         </div>
       </div>
-    </HeaderStyles>
+      <style scoped>{`
+        div.headbanner {
+          background-color: #000000;
+          padding: 0;
+        }
+        div.carousel-item img {
+          width: 100%;
+          height: auto;
+        }
+        img.imgheader {
+          width: 100%;
+          height: auto;
+        }
+        .banner a:hover {
+          text-decoration: none;
+        }
+        .banner h1 {
+          color: #ffffff;
+          margin: 50px;
+          margin-bottom: 2px;
+          font-size: 1.6em;
+          font-family: Quicksand, Arial, Helvetica, sans-serif;
+          font-weight: 300;
+        }
+        .banner h2 {
+          color: #ffffff;
+          margin: 50px;
+          margin-top: 2px;
+          font-size: 1.1em;
+          font-family: Quicksand, Arial, Helvetica, sans-serif;
+          font-weight: 300;
+        }
+        @media only screen and (max-width: 650px) {
+          .banner h1 {
+            margin: 15px 20px 4px 20px;
+          }
+          .banner h2 {
+            margin: 2px 20px 10px 20px;
+          }
+        }
+      `}</style>
+    </div>
   );
 }
--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -1,36 +1,8 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import { Navbar, Nav } from 'react-bootstrap';
-import styled from 'styled-components';
-
-const NavigationStyles = styled.div`
-  .navbar {
-    background-color: #343a40;
-    color: #ffffff;
-  }
-  .navcontainer {
-    background-color: #343a40;
-  }
-  .navbar a {
-    color: #ffffff;
-  }
-  .navbar-brand {
-    font-size: 1em;
-  }
-  .nav-item {
-    padding: 4px 10px;
-  }
-  .nav-item a {
-    color: #aaaaaa;
-  }
-  .nav-item a.active {
-    color: red;
-  }
-  .nav-item a:hover {
-    text-decoration: none;
-    color: #eeeeee;
-  }
-`;
+//import styled from 'styled-components';
+//const NavigationStyles = styled.div``;
 
 export default function NavBar() {
   const data = useStaticQuery(graphql`
@@ -59,53 +31,79 @@ export default function NavBar() {
   const galleries = data.collections.nodes;
   const textpages = data.textpages.nodes;
   return (
-    <NavigationStyles>
-      <div
-        className="container-fluid"
-        style={{ backgroundColor: `#343a40`, padding: `0` }}
-      >
-        <div className="container">
-          <Navbar collapseOnSelect bg="dark" variant="dark" expand="md">
+    <div
+      className="container-fluid"
+      style={{ backgroundColor: `#343a40`, padding: `0` }}
+    >
+      <div className="container">
+        <Navbar collapseOnSelect bg="dark" variant="dark" expand="md">
+          <Nav>
+            <Nav.Item>
+              <Link to="/" activeStyle={{ color: 'white' }}>
+                Home
+              </Link>
+            </Nav.Item>
+          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
             <Nav>
+              {galleries.map(el => (
+                <Nav.Item key={el.id}>
+                  <Link
+                    to={`/gallery/${el.slug.current}`}
+                    activeClassName="active"
+                  >
+                    {el.name}
+                  </Link>
+                </Nav.Item>
+              ))}
+              {textpages.map(el => (
+                <Nav.Item key={el.id}>
+                  <Link to={`/${el.slug.current}`} activeClassName="active">
+                    {el.name}
+                  </Link>
+                </Nav.Item>
+              ))}
               <Nav.Item>
-                <Link to="/" activeStyle={{ color: 'white' }}>
-                  Home
+                <Link to="/contact" activeClassName="active">
+                  Contact
                 </Link>
               </Nav.Item>
             </Nav>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse
-              id="basic-navbar-nav"
-              className="justify-content-end"
-            >
-              <Nav>
-                {galleries.map(el => (
-                  <Nav.Item key={el.id}>
-                    <Link
-                      to={`/gallery/${el.slug.current}`}
-                      activeClassName="active"
-                    >
-                      {el.name}
-                    </Link>
-                  </Nav.Item>
-                ))}
-                {textpages.map(el => (
-                  <Nav.Item key={el.id}>
-                    <Link to={`/${el.slug.current}`} activeClassName="active">
-                      {el.name}
-                    </Link>
-                  </Nav.Item>
-                ))}
-                <Nav.Item>
-                  <Link to="/contact" activeClassName="active">
-                    Contact
-                  </Link>
-                </Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
-    </NavigationStyles>
+      <style scoped>{`
+        .navbar {
+          background-color: #343a40;
+          color: #ffffff;
+        }
+        .navcontainer {
+          background-color: #343a40;
+        }
+        .navbar a {
+          color: #ffffff;
+        }
+        .navbar-brand {
+          font-size: 1em;
+        }
+        .nav-item {
+          padding: 4px 10px;
+        }
+        .nav-item a {
+          color: #aaaaaa;
+        }
+        .nav-item a.active {
+          color: red;
+        }
+        .nav-item a:hover {
+          text-decoration: none;
+          color: #eeeeee;
+        }
+      `}</style>
+    </div>
   );
 }
